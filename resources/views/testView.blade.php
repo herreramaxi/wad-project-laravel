900976e95f67255ebcb224a57c16055961c9a069
Maxi <herreramaxi@gmail.com>
2020-11-26 00:55:16 +0000

    adding php syntax on test view for displaying customers

--- resources/views/testView.blade.php
+++ resources/views/testView.blade.php
@@ -1,10 +1,17 @@
 <html>
 <body>
-<h1>hello</h1>
+<h1>Users using blade syntax</h1>
 
 @foreach ($users as $user)
     <p>This is user {{ $user->customername }}</p>
 @endforeach
 
+<h1>Users using php syntax</h1>
+
+<?php
+   foreach ($users as $user2) {
+    echo 'This is user '.  $user2->customername . ' </p>';
+   }
+?>
 </body>
 </html>
