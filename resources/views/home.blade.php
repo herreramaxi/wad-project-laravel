9d2b9cf670a25c51194818898136de258b89e68b
Maxi <herreramaxi@gmail.com>
2020-12-11 00:50:05 +0000

    testing access to json file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,26 @@
 @extends('shared.layout')
 @section('content')
 <h1>test</h1>
-
+<br>
 @php
 // $path = storage_path() . "/app/json/navData.json"; // ie: /var/www/laravel/app/storage/json/filename.json
 echo getcwd ( ) ;
-$path = "storage/navData.json";
+echo '<br>';
+$path = "navData.json";
  echo $path;
-//  $json = json_decode(file_get_contents($path), true);
+ echo '<br>';
 
+ $json = json_decode(file_get_contents($path), true);
 
-// $output = "<ul>";
-//     foreach ($json['items'] as $movie) {
-//         $output .= "<li>"
-//             . $movie['name'] 
-//             . "</li>";
-//     }
-//     $output .= "</ul>";
+$output = "<ul>";
+    foreach ($json['items'] as $movie) {
+        $output .= "<li>"
+            . $movie['name'] 
+            . "</li>";
+    }
+    $output .= "</ul>";
 
-//     echo $output;
+    echo $output;
 
 @endphp
 
