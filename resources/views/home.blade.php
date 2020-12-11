f8af5a0fd70dfcea5f895fde5c9b69dac50e5dae
Maxi <herreramaxi@gmail.com>
2020-12-11 00:33:17 +0000

    testing reading from json

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,25 @@
 @extends('shared.layout')
 @section('content')
 <h1>test</h1>
+
+@php
+// $path = storage_path() . "/app/json/navData.json"; // ie: /var/www/laravel/app/storage/json/filename.json
+echo getcwd ( ) ;
+$path = "storage/navData.json";
+ echo $path;
+ $json = json_decode(file_get_contents($path), true);
+
+
+$output = "<ul>";
+    foreach ($json['items'] as $movie) {
+        $output .= "<li>"
+            . $movie['name'] 
+            . "</li>";
+    }
+    $output .= "</ul>";
+
+    echo $output;
+
+@endphp
+
 @stop
