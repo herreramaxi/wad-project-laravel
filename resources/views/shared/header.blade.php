3371cc78675f7572b10a1f79ad99f53841ae9e9b
Maxi <herreramaxi@gmail.com>
2020-12-11 17:17:55 +0000

    rendering nav bar from json definition navData.json

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -7,15 +7,26 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="productsClient">Menus</a>
+            @php
+            $path = "navData.json";
+            $json = json_decode(file_get_contents($path), true);
+            @endphp
+
+            @isset( $json )
+
+            @foreach ($json['items'] as $item)
+
+            @if( $item['name']!= null && $item['href'] !=null )
+            <li class="nav-item {{ (request()->is($item['href'])) ? 'active' : '' }}">
+                <a class="nav-link" href="/{{$item['href']}}">{{$item['name']}}</a>
             </li>
-            <li class="nav-item {{ (request()->is('products')) ? 'active' : '' }}">
-                <a class="nav-link" href="/products">Admin</a>
-            </li>
-            <li class="nav-item {{ (request()->is('home')) ? 'active' : '' }}">
-                <a class="nav-link" href="/home">Test</a>
-            </li>
+            @endif
+
+            @endforeach
+
+
+            @endisset
+
         </ul>
     </div>
 </nav>
