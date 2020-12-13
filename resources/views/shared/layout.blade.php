6e144812e464ac0ff95d5078668d3591cd1b792b
Valeria Etchudez <valeria.etchudez@gmail.com>
2020-12-13 18:36:15 +0000

    INTERNAL: Updated common header and layout

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -2,31 +2,30 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <title>Food delivery app</title> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Food Delivery - App</title>
+    <!-- The filemtime is to append a timestamp for the last time the stylesheet was updated to automate cache busting from CloudFlare -->
+    <link rel="stylesheet" href="https://themes.getbootstrap.com/wp-content/themes/bootstrap-marketplace/style.css?ver=1590611604">
 </head>
 
 <body>
+    
+    <!-- Header -->
+    @include('shared.header')
+
+    <!-- Content page -->
     <div class="container">
-
-        <header class="row">
-            @include('shared.header')
-        </header>
-
-        <div id="main" class="row">
-
-            @yield('content')
-
-        </div>
-
-        <footer class="row">
-            {{-- @include('shared.footer') --}}
-        </footer>
-
+        @yield('content')
     </div>
+
+    <!-- Footer -->
+    <footer class="section section_footer">
+        {{-- @include('shared.footer') --}}
+    </footer>
 </body>
 
 </html>
