4db6387e1fe1a76021b627a1496bb8275839519e
Valeria Etchudez <valeria.etchudez@gmail.com>
2020-12-19 22:06:02 +0000

    INTERNAL: Updated index view for productsClient

--- resources/views/productsClient/index.blade.php
+++ resources/views/productsClient/index.blade.php
@@ -1,59 +1,42 @@
 @extends('shared.layout')
 @section('content')
-<script src="{{ asset('js/productsClient/index.js') }}" defer></script>
+<!-- Import styles for menu -->
 <link href="{{ asset('css/productsClient/index.css') }}" rel="stylesheet">
+<!-- Import typeahead.js and typeahead boostrap-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" defer />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js" defer></script>
+<!-- Import index.js -->
+<script src="{{ asset('js/productsClient/index.js') }}" defer></script>
 <!-- WELCOME
-  ================================================== -->
-<div class="container">
-  <section class="section section_welcome" data-parallax="scroll" data-image-src="assets/img/1.jpg">
-    <div class="row">
-      <div class="col">
+                  ================================================== -->
+<div class="container" id="productsClientContainer">
+    <section class="section section_welcome" data-parallax="scroll" data-image-src="assets/img/1.jpg">
+        <div class="row">
+            <div class="col">
 
-        <!-- Heading -->
-        <h2 class="section__heading text-center">
-          <em>Our menu</em>
-        </h2>
-        <p class="section__subheading text-center">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias veritatis nisi, consequatur, laborum libero a neque ducimus. Porro rem illum quo nostrum quisquam asperiores, blanditiis, consectetur. Possimus facilis velit, voluptatibus!
-        </p>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="searchBox">
-          <a href="#" class="btn btn-primary" tabindex="1" role="button" id="searchBtn">Search</a>
-          <a href="#" class="btn btn-secondary" tabindex="1" role="button" id="clearLnk">Clear Search</a>
-        </form>
-      </div> <!-- / .row -->
-  </section>
-  <!-- MENU
-================================================== -->
-  <section class="section section_menu section_border_bottom">
-    <!-- MENU 
-	================================================== -->
-    <div class="row section_menu__grid" id="menu_images">
-      @foreach ($products as $product)
-      <div class="col-md-6 section_menu__grid__item mains">
-        <div class="section_menu__item">
-          <div class="row">
-            <div class="col-3 align-self-center">
-              <div class="section_menu__item__img">
-                <img src="{{ getImageSrc($product) }}" alt="food_img">
-              </div>
-            </div>
-            <div class="col-7">
-              <h4 class="item-content">{{ $product-> name }}</h4>
-              <p class="item-content">
-                {{ $product-> description }}
-              </p>
-            </div>
-            <div class="col-2">
-              <div class="section_menu__item__price text-center item-content">
-                {{ $product->price }}
-              </div>
-            </div>
-          </div> <!-- / .row -->
-        </div>
-      </div>
-      @endforeach
-    </div> <!-- / .row -->
+                <!-- Heading -->
+                <h2 class="section__heading text-center">
+                    <em>Our menu</em>
+                </h2>
+                <p class="section__subheading text-center">
+                    "Inspired by a love of Argentine food, wine, vivacious lifestyle and rich culture, our menu follows the Argentine tradition of sharing food."
+                </p>
+                <form class="d-flex"  filter-attr="{{ url('autocomplete') }}" data-attr="{{route('productsClient.show','show')}}" id="searchForm">
+                    <input id="search" name="search" type="text" class="form-control me-2" aria-label="Search"
+                        placeholder="Search" />
+                </form>
+            </div> <!-- / .row -->
+    </section>
+    <!-- MENU
+                ================================================== -->
+    <section class="section section_menu section_border_bottom">
+        <!-- MENU 
+                 ================================================== -->
+        <div class="row section_menu__grid" id="menu_images">
+            @foreach ($products as $product)
+                @include('productsClient.show', ['product' => $product])
+            @endforeach
+        </div> <!-- / .row -->
 </div> <!-- / .container -->
 </section>
 <br>
