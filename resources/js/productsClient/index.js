26b0c84416e08b7572290d23d8f97ebfdd8ee0c6
Valeria Etchudez <valeria.etchudez@gmail.com>
2020-12-19 22:06:46 +0000

    INTERNAL: Updated productsClient js

--- resources/js/productsClient/index.js
+++ resources/js/productsClient/index.js
@@ -1,35 +1,67 @@
-$(document).ready(function () {
-    console.log("ready!");
+$(function () {
+    $(document).autocomplete();
+});
 
-    $("#clearLnk").on("click", function () {
-        var searchBox = $('#searchBox');
-        searchBox.val("");
-        $('.section_menu__grid__item').each(function () {
-            var boxMenu = $(this);
-            boxMenu[0].style.display = "";
-        });
+function setPartialView(href, itemName) {
+    event.preventDefault();
+    $.ajax({
+        url: href,
+        data: {
+            name: itemName
+        },
+        beforeSend: function () {
+        },
+        success: function (data) {
+            let doc = document.implementation.createHTMLDocument("partial view doc");
+            doc.documentElement.innerHTML = data;
+            let partialViewBody = doc.documentElement.childNodes[2].innerHTML;
+            document.documentElement.childNodes[2].innerHTML = partialViewBody;
+        },
+        error: function (xhr, status, error) {
+            console.log(status);
+            console.log(error);
+
+            if (status == 404) {
+                showErrorMessage("View not found");
+            } else {
+                showErrorMessage("Error when trying to load create view");
+            }
+        },
+        complete: function (jqXHR, textStatus) {
+        }
     });
+}
 
-    $("#searchBtn").on("click", function () {
-        event.preventDefault();
-        var searchBox = $('#searchBox');
-        var searchText = searchBox.val();
-        searchText = searchText.toLowerCase();
-        searchText = searchText.replace(/\s+/g, '');
-        $('.section_menu__grid__item').each(function () {
-            var foundInBox = false;
-            var boxMenu = $(this);
-            boxMenu.each(function () {
-                var currentText = $(this).text();
-                if (currentText.toLowerCase().replace(/\s+/g, '').indexOf(searchText) !== -1) {
-                    foundInBox = true;
+$.fn.autocomplete = function () {
+    {
+        $('#productsClientContainer').on('click', '#search', function (e) {
+            e.preventDefault();
+            $('#search').typeahead({
+                highlight: true,
+                suggestions:false,
+                hint:false,
+                minLength: 1,
+                updater: function (item) {
+                    let href = $("#searchForm").attr("data-attr");
+                    setPartialView(href, item.name);
+                    return false;
+                },
+                source: function (term, process) {
+                    let route = $("#searchForm").attr("filter-attr");
+                    return $.get(route, {
+                        term: term
+                    }, function (data) {
+                        return process(data);
+                    });
+
                 }
             });
-            if (foundInBox) {
-                boxMenu[0].style.display = "";
-            } else {
-                boxMenu[0].style.display = "none";
-            }
         });
-    });
+    }
+}
+
+//When Ajax completes we need to unbind all the events from the document ready, otherwise the script won't run for a second time
+$(document).ajaxStop(function () {
+
+    $(document).autocomplete();
 });
