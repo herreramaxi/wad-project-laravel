e3f38b4c01fa3f31cd267d17a9f5ae5174558308
Valeria Etchudez <valeria.etchudez@gmail.com>
2020-12-19 22:04:45 +0000

    INTERNAL: Updated controller and js for productsClient page

--- app/Http/Controllers/ProductsClientController.php
+++ app/Http/Controllers/ProductsClientController.php
@@ -1,14 +1,37 @@
 <?php
+
 namespace App\Http\Controllers;
+
 namespace App\Http\Controllers;
+
 use App\Models\Product;
+use Exception;
+use Illuminate\Http\Request;
 
 class ProductsClientController extends Controller
 {
-    public function getIndex()
+    public function index()
     {
-        $products = Product::orderBy('name')->get();
-
+        $products = Product::all();
         return view('productsClient.index', compact('products'));
     }
+
+    public function show(Request $request)
+    {
+        $products = null;
+        if($request->has('name'))
+        {
+            $products = Product::all();
+        }
+        $products = Product::where('name', 'LIKE', '%' . request('name') . '%')->get();
+        return view('productsClient.index', compact('products'));
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->get('term');
+        $products = Product::where('name', 'LIKE', '%' . $search . '%')->get('name');
+        return response()->json($products);
+    }
+
 }
