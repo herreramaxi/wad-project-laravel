4566f14fa135da8fd92a38fd4096695fd6dd0d7f
Maxi <herreramaxi@gmail.com>
2020-12-31 01:33:50 +0000

    adding a protection when showing and hiding the spinner

--- resources/js/spinner.js
+++ resources/js/spinner.js
@@ -1,6 +1,10 @@
 module.exports = {
     show: function() {
         $("#modalSpinner").modal("show");
+        
+        setTimeout(function () {
+            $("#modalSpinner").modal("hide");
+        }, 2000);
     },
     hide: function() {
         setTimeout(function() {
