e9279c326a29a6dd344025acb2eb438b2baff3a1
Maxi <herreramaxi@gmail.com>
2021-01-04 02:04:38 +0000

    fixing issues on alert messages. Fix issue on preview image on create view

--- resources/js/products/edit.js
+++ resources/js/products/edit.js
@@ -18,6 +18,7 @@ function readURL(input) {
 }
 
 $(document).on("change", "#inputFileId", function(event) {
+    event.preventDefault();
     event.stopPropagation();
     readURL(this);
 });
--- resources/views/shared/alertMessages.blade.php
+++ resources/views/shared/alertMessages.blade.php
@@ -1,12 +1,12 @@
 <div id="alertMessagesArea">
     <div id="errorResult">
-        <div id="errorMessageAlert" class="alert alert-danger alert-dismissible" role="alert">
+        <div id="errorMessageAlert" class="alert alert-danger alert-dismissible" style="display: none;" role="alert">
             <div id="errorMessageDiv"></div>
             <button id="ErrorMessageButtonClose" type="button" class="close" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
-        <div id="successMessageAlert" class="alert alert-success alert-dismissible" role="alert">
+        <div id="successMessageAlert" class="alert alert-success alert-dismissible" style="display: none;" role="alert">
             <div id="successMessageDiv"></div>
             <button id="successMessageButtonClose" type="button" class="close" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
